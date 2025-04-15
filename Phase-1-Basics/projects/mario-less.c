f91d02f7ab3c513eac8836e5decb3289ff9f9a29
Th3Hacker <hamza.annane.official@gmail.com>
2025-04-15 11:56:22 +0100

    projects

---
+++ Phase-1-Basics/projects/mario-less.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int height;
+
+    do {
+
+        // Prompt the user for the pyramid's height
+        printf("height: ");
+        scanf("%d",&height);
+
+    } while(height < 1 || height > 8);
+
+    for (int i = 1 ; i <= height; i++)
+    {
+        for (int j = 1 ; j <= height - i; j++)
+        {
+            printf(" ");
+        }
+        
+        for (int k = 1 ; k <= i ; k++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+
+}
